15f265619282aa18e98e1d0df12b8d1a350b913f
Dereckson <dereckson@espace-win.org>
2013-10-31 18:17:24 +0100

    New API: /design/kibaone/accents
        Purpose: Returns a JSON document with a listing of Kiba One accents.
        URL: http://api.wolfplex.org/design/kibaone/accents
        Method: GET
        Parameters: none
        Format: An array of object, each one having 3 properties:
            name: the accent name
            accent: the background color
            headingColor: the heading color

---
+++ design/kibaone/accents/index.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Wolfplex Kiba One design available accents
+ *
+ * Outputs an array of accents:
+ * name:    		accent name
+ * accent:  		background color
+ * headingColor:	text color
+ */
+
+$white = '#ffffff'; //We currently don't consider a grayish white for cyan compliance.
+$black = '#000000'; //We currently don't consider a dark gray for citron compliance.
+$defaultHeadingsColor = $white;
+
+$accents = [
+	// Main colors
+	[ 'black', '#000000', '#4b4b50' ],
+	[ 'zedgray', '#343434' ],
+	[ 'bluegray', '#4b4b50' ],
+
+	// Colored accents
+	[ 'cyan', '#2ba6cb' ],
+	[ 'lime', '#e7fb03', $black ],
+	[ 'magenta', '#f608b0' ],
+	[ 'craie', '#f3f3f3', $black ],
+	[ 'grenade', '#f34723' ],
+	[ 'sorbier', '#fb8507' ],
+	[ 'purple', '#998cfb' ],
+	[ 'citron', '#fcff00', $black ],
+	[ 'blueribbon', '#1d62ff' ],
+	[ 'red', '#f60000' ],
+];
+
+$areas = [];
+foreach ($accents as $accent) {
+	$areas[] = [
+		'name'         => $accent[0],
+		'accent'       => $accent[1],
+		'headingColor' => count($accent) < 3 ? $defaultHeadingsColor : $accent[2]
+	];
+}
+
+header("Content-Type: application/json");
+echo json_encode($areas);
