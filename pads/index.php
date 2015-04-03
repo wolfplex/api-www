76d12f1bc6dc57ab47938360db7804882cb89e16
Sébastien Santoro <dereckson@espace-win.org>
2015-04-03 21:46:21 +0000

    Store the Etherpad API key as a secret.
    Currently, the Etherpad API key were public, with the rationale the
    installation is a public one, everybody can anonymously or with a name
    make edits, destroy pads, create new pads, etc.
    
    Nasqueron is interested to get the potential to integrate other applications
    with Etherpad, using groups, sessions and users objects to make pads spawned
    by 3rd party application private.
    
    A private API key has so been regenerated, and we now store it as a secret.

--- pads/index.php
+++ pads/index.php
@@ -1,12 +1,14 @@
 <?php
+    require '../_includes/SecurityData.php';
+
     /**
      * Gets the pads
      *
      * @return Array the pads list
      */
     function get_pads () {
-        //TODO: move API key in config
-        $url = "http://pad.wolfplex.be/api/1.2.1/listAllPads?apikey=U67yKTUDzz19fEHyrAuNN4zUnKxbOJZu";
+        $key = SecurityData::getSecret('etherpad.api.key');
+        $url = "http://pad.wolfplex.be/api/1.2.1/listAllPads?apikey=" . $key;
         $api_reply = json_decode(file_get_contents($url));
         if ($api_reply->code == 0) {
             return $api_reply->data->padIDs;
