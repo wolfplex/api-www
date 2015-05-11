551bc1ee1a4779ca84ee3cbfa6ed7fce3be2069e
Sébastien Santoro <dereckson@espace-win.org>
2015-05-11 09:06:38 +0000

    Offers a clear error message when Composers dependencies are missing.
    We check if a vendor/autoload.php file exists. If not, we print a error
    message, then exit.
    
    **Error message**
    
    Please install Composer if needed, then run 'make'
    or directly 'composer install'.

--- _includes/core.php
+++ _includes/core.php
@@ -1,7 +1,11 @@
 <?php
 
 // Composer deps
-require '../vendor/autoload.php';
+define('AUTOLOAD', '../vendor/autoload.php');
+if (!file_exists(AUTOLOAD)) {
+	die("Please install Composer if needed, then run 'make' or directly 'composer install'.");
+}
+require AUTOLOAD;
 
 // log4php configuration
 if (isset($_SERVER['LOGGER_CONFIG']) && file_exists($_SERVER['LOGGER_CONFIG'])) {
