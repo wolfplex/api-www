83e5c5cd11a3b2a9ad34d5160e39f4c59a651374
Sébastien Santoro <dereckson@espace-win.org>
2015-05-28 15:07:11 +0000

    More robust Etherpad API querying and error handling
    We now intercept errors from file_get_contents(), like this one:
    failed to open stream: HTTP request failed! HTTP/1.1 502 Bad Gateway
    
    This will be logged, and an empty array will be served.

--- pads/index.php
+++ pads/index.php
@@ -8,10 +8,25 @@
      * @return Array the pads list
      */
     function get_pads () {
+        //Queries API
+        $apiUrl = "http://pad.wolfplex.be/api/1.2.1/listAllPads";
         $key = SecurityData::getSecret('etherpad.api.key');
-        $url = "http://pad.wolfplex.be/api/1.2.1/listAllPads?apikey=" . $key;
-        $api_reply = json_decode(file_get_contents($url));
-        if ($api_reply->code == 0) {
+        $url = $apiUrl . "?apikey=" . $key;
+        try {
+            $api_reply = json_decode(file_get_contents($url));
+        } catch (ErrorException $ex) {
+            //Logs error, but removes sensitive information
+            $errorMessage = str_replace($url, "$apiUrl?…", $ex->getMessage());
+            Logger::getLogger("pads")->fatal($errorMessage);
+            return [];
+        }
+
+        //Check reply sanity
+        if (!property_exists('code', $api_reply)) {
+            Logger::getLogger("pads")->fatal("Unexpected API reply.");
+            return [];
+        }
+        if ($api_reply->code === 0) {
             return $api_reply->data->padIDs;
         }
 
