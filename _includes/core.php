1dc0d611ceb99c0e980275bb2db8871318c048c5
Sébastien Santoro <dereckson@espace-win.org>
2015-05-28 15:01:19 +0000

    PHP errors are now handled as exception
    We set an error handler to turn any unsuppressed PHP error
    into an exception, including notices, warning, strict, etc.
    
    **Background**
    
    We want to catch and log file_get_contents issues, like this one from /pads:
    failed to open stream: HTTP request failed! HTTP/1.1 502 Bad Gateway
    
    **How to use it**
    
    This allows constructions like:
    
        try {
            // Code
        } catch (ErrorException $ex) {
            // Handles exception
        }

--- _includes/core.php
+++ _includes/core.php
@@ -7,6 +7,17 @@ if (!file_exists(AUTOLOAD)) {
 }
 require AUTOLOAD;
 
+// Turns all PHP errors into exceptions
+// See http://stackoverflow.com/a/1241751/1930997
+set_error_handler(function ($errno, $errstr, $errfile, $errline, array $errcontext) {
+	// Error suppressed with the @-operator shouldn't transformed in exceptions.
+	if (error_reporting() === 0) {
+		return false;
+	}
+
+	throw new ErrorException(trim($errstr), 0, $errno, $errfile, $errline);
+});
+
 // log4php configuration
 if (isset($_SERVER['LOGGER_CONFIG']) && file_exists($_SERVER['LOGGER_CONFIG'])) {
 	Logger::configure($_SERVER['LOGGER_CONFIG']);
